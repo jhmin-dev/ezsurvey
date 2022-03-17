a8f3c646560d86e7376562f0914a45755d77849b
jhmin-dev <jhmin.dev@gmail.com>
2022-03-17 16:10:21 +0900

    프로젝트 초기화

---
+++ README.md
@@ -0,0 +1,13 @@
+# ezsurvey
+
+- 간편하게 설문조사를 제작, 공유하는 사이트를 Spring Boot 기반으로 로컬에서 개발하는 1인 프로젝트
+	+ 2022년 3월 14일부터 4월 11일까지 진행
+
+## 개발 환경
+
+- Spring Boot 2.6.4.RELEASE
+- Java 11
+- JPA
+- Oracle Cloud Autonomous Database(19c)
+- Lombok
+- Gradle
---
+++ src/main/java/io/ezsurvey/EZSurveyApplication.java
@@ -0,0 +1,13 @@
+package io.ezsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EZSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EZSurveyApplication.class, args);
+	}
+
+}
