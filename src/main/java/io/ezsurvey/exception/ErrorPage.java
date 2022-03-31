f495e0693668a71044f8f069c994ef54d3b780d7
jhmin-dev <jhmin.dev@gmail.com>
2022-04-01 02:18:21 +0900

    fix: BasicErrorController에서 처리하는 에러 페이지의 레이아웃 깨짐 수정
    BasicErrorController는 Tiles 설정명이 아니라 error.jsp를 직접 호출하기 때문에, 충돌을 피하기 위해
    GlobalExceptionHandler에서 호출하는 Tiles 설정명 및 해당 설정명에서 사용하는 JSP 파일명을
    exception으로 변경(BasicErrorController는 Whitelabel Error Page를 생성)
    TilesConfig에서 setOrder(0)은 효과가 없었고, WebMvcConfig를 만들어
    InternalResourceViewResolver에 setOrder(1)을 하면 Circular view path error
    발생

--- src/main/java/io/ezsurvey/exception/ErrorPage.java
+++ src/main/java/io/ezsurvey/exception/ErrorPage.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorPage {
-	DEFAULT_VIEW("/error");
+	DEFAULT_VIEW("/exception");
 	
 	public final String name;
 }
