fb0fd40b832fc2c69500f74168bc5ab0dc0785f0
jhmin-dev <jhmin.dev@gmail.com>
2022-04-01 11:57:17 +0900

    chore: BasicErrorController의 errorHtml()을 재정의하기 위해 상속 클래스 추가
    https://supawer0728.github.io/2019/04/04/spring-error-handling/
    errorHtml()에서 Tiles 설정명을 호출하도록 재정의(View에는 상태 코드만 전달됨)

---
+++ src/main/java/io/ezsurvey/web/CustomErrorController.java
@@ -0,0 +1,39 @@
+package io.ezsurvey.web;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("${server.error.path:${error.path:/error}}")
+public class CustomErrorController extends BasicErrorController {
+
+	public CustomErrorController(ErrorAttributes errorAttributes, ServerProperties serverProperties,
+			List<ErrorViewResolver> errorViewResolvers) {
+		super(errorAttributes, serverProperties.getError(), errorViewResolvers);
+	}
+	
+	@Override
+	@RequestMapping(produces = MediaType.TEXT_HTML_VALUE)
+	public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+		HttpStatus status = getStatus(request);
+		Map<String, Object> model = Collections
+				.unmodifiableMap(getErrorAttributes(request, getErrorAttributeOptions(request, MediaType.TEXT_HTML)));
+		response.setStatus(status.value());
+		ModelAndView modelAndView = resolveErrorView(request, response, status, model);
+		return (modelAndView != null) ? modelAndView : new ModelAndView("/exception", model);
+	}
+}
