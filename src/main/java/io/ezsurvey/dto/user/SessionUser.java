96a8aabac5ed36896d818a91071b34c0ce1bcdbb
jhmin-dev <jhmin.dev@gmail.com>
2022-04-05 00:24:04 +0900

    build: Spring Session과 Redis 사용 설정하고 SessionUser에 serialVersionUID 추가

--- src/main/java/io/ezsurvey/dto/user/SessionUser.java
+++ src/main/java/io/ezsurvey/dto/user/SessionUser.java
@@ -4,9 +4,15 @@ import java.io.Serializable;
 
 import io.ezsurvey.entity.user.User;
 import lombok.Getter;
+import lombok.ToString;
 
+@ToString
 @Getter
-public class SessionUser implements Serializable { // 세션에 인증된 사용자 정보를 저장하기 위해 직렬화한 클래스
+public class SessionUser implements Serializable {
+	// 생략시 Redis에 세션을 저장하는 경우 SerializationException: Cannot deserialize 발생
+	private static final long serialVersionUID = 4286759114371999351L;
+	
+	// 세션에 인증된 사용자 정보를 저장하기 위해 직렬화한 클래스
 	private Long member;
 	private String name;
 	private String profileURL;
