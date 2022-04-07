9a89a7bdc10ec7d60af016f0f950cbb44503b298
jhmin-dev <jhmin.dev@gmail.com>
2022-04-07 09:20:25 +0900

    chore: 설문조사 추가시 null이 아닌 필드에 대해 불필요하게 null 여부 검사하는 단계 제거

--- src/main/java/io/ezsurvey/service/survey/SurveyCUDService.java
+++ src/main/java/io/ezsurvey/service/survey/SurveyCUDService.java
@@ -37,10 +37,7 @@ public class SurveyCUDService {
 		
 		// 필수 필드에 값 저장
 		serviceDTO.setStatus(Status.BEFORE); // 설문조사 생성 시에는 배포 상태가 항상 배포 전으로 설정되어야 함
-		if(serviceDTO.getVisibility()==null) { // 사용자가 공개 여부를 별도로 설정하지 않은 경우
-			serviceDTO.setVisibility(Visibility.HIDDEN); // 비공개로 설정
-		}
-		else if(serviceDTO.getVisibility()==Visibility.LINK_ONLY) { // 일부 공개로 설정한 경우
+		if(serviceDTO.getVisibility()==Visibility.LINK_ONLY) { // 일부 공개로 설정한 경우
 			serviceDTO.setShared(UUID.randomUUID().toString()); // 설문조사 UUID 값을 생성하여 저장
 		}
 		
