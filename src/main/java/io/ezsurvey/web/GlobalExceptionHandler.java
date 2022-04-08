789610c91b41366547688a5860fc952bc8f09efe
jhmin-dev <jhmin.dev@gmail.com>
2022-04-08 10:06:03 +0900

    refactor: 불필요하게 분리된 ExceptionHandlingUtil을 제거
    ExceptionHandlingUtil의 static 메서드는 GlobalExceptionHanlder에서만 호출되므로,
    GlobalExceptionHanlder 내의 instance 메서드로 변경

--- src/main/java/io/ezsurvey/web/GlobalExceptionHandler.java
+++ src/main/java/io/ezsurvey/web/GlobalExceptionHandler.java
@@ -3,63 +3,53 @@ package io.ezsurvey.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import io.ezsurvey.exception.BusinessException;
 import io.ezsurvey.exception.DeletedSurveyException;
 import io.ezsurvey.exception.EntityNotFoundException;
+import io.ezsurvey.exception.ErrorCode;
 import io.ezsurvey.exception.ErrorPage;
 import io.ezsurvey.exception.InvalidSurveyOwnerException;
 import io.ezsurvey.exception.InvalidSurveyStatusException;
 import io.ezsurvey.exception.InvalidSurveyVisibilityException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @ControllerAdvice // 모든 컨트롤러에 적용
 public class GlobalExceptionHandler {
-	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
 	@ExceptionHandler(value = EntityNotFoundException.class)
 	public ModelAndView handleEntityNotFoundException(EntityNotFoundException e, HttpServletRequest request) {
-		logger.error("handleEntityNotFoundException", e);
-		
-		return ExceptionHandlingUtil.getModelAndView(e, request);
+		return getModelAndViewFromBusinessException(e, request);
 	}
 	
 	@ExceptionHandler(value = DeletedSurveyException.class)
 	public ModelAndView handleDeletedSurveyException(DeletedSurveyException e, HttpServletRequest request) {
-		logger.error("handleDeletedSurveyException", e);
-		
-		return ExceptionHandlingUtil.getModelAndView(e, request);
+		return getModelAndViewFromBusinessException(e, request);
 	}
 	
 	@ExceptionHandler(value = InvalidSurveyVisibilityException.class)
 	public ModelAndView handleInvalidSurveyVisibilityException(InvalidSurveyVisibilityException e, HttpServletRequest request) {
-		logger.error("handleInvalidSurveyVisibilityException", e);
-		
-		return ExceptionHandlingUtil.getModelAndView(e, request);
+		return getModelAndViewFromBusinessException(e, request);
 	}
 	
 	@ExceptionHandler(value = InvalidSurveyOwnerException.class)
 	public ModelAndView handleInvalidSurveyOwnerException(InvalidSurveyOwnerException e, HttpServletRequest request) {
-		logger.error("handleInvalidSurveyOwnerException", e);
-		
-		return ExceptionHandlingUtil.getModelAndView(e, request);
+		return getModelAndViewFromBusinessException(e, request);
 	}
 	
 	@ExceptionHandler(value = InvalidSurveyStatusException.class)
 	public ModelAndView handleInvalidSurveyStatusException(InvalidSurveyStatusException e, HttpServletRequest request) {
-		logger.error("handleInvalidSurveyStatusException", e);
-		
-		return ExceptionHandlingUtil.getModelAndView(e, request);
+		return getModelAndViewFromBusinessException(e, request);
 	}
 	
 	@ExceptionHandler(value = Exception.class)
 	public ModelAndView defaultErrorHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception {
-		logger.error("defaultErrorHandler", e);
+		log.error("defaultErrorHandler", e);
 		
 		// If the exception is annotated with @ResponseStatus rethrow it and let the framework handle it
 		if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
@@ -72,6 +62,20 @@ public class GlobalExceptionHandler {
 		mav.addObject("url", request.getRequestURL());
 		mav.addObject("exception", e);
 		mav.addObject("status", response.getStatus());
+		mav.setViewName(ErrorPage.DEFAULT_VIEW.getName());
+		
+		return mav;
+	}
+	
+	private <E extends BusinessException> ModelAndView getModelAndViewFromBusinessException(E e, HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		ErrorCode errorCode = e.getErrorCode();
+		
+		mav.addObject("url", request.getRequestURL());
+		mav.addObject("exception", e);
+		mav.addObject("errorCode", errorCode);
+		mav.addObject("status", errorCode.getStatus());
+		
 		mav.setViewName(ErrorPage.DEFAULT_VIEW.getName());
 		
 		return mav;
